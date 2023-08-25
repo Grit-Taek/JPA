effb372482cc6e0498bd394de21b3a038e37e45f
Oh Hyuntaek <01077275247a@gmail.com>
2023-08-25 15:57:41 +0900

    jpa inheritance

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+}
