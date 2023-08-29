5074275285428b5275dd5cd3d5880e525f1a94c8
Oh Hyuntaek <01077275247a@gmail.com>
2023-08-29 18:11:03 +0900

    join tdd complete

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
