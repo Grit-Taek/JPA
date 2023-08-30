11a824ed7371de54b5eead7295a9051d5265b6a2
Oh Hyuntaek <01077275247a@gmail.com>
2023-08-30 23:10:37 +0900

    create order domain

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -31,4 +32,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member1.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member1);
+
+        //then
+        fail("예외가 발생해야 한다.");
+
+    }
 }
