812caf4d5cca7c57c2cd3d80d7e1cd046468ea3c
Oh Hyuntaek <01077275247a@gmail.com>
2023-08-31 17:30:49 +0900

    member domain complete

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,20 +1,29 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
 
-    private final EntityManager em;
+    @PersistenceContext
+    EntityManager em;
+
     public void save(Order order) {
         em.persist(order);
     }
     public Order findOne(Long id) {
         return em.find(Order.class,id);
     }
+
 }
